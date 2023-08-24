28bfc521daa90235cce9be574e64968b8a6e6de9
Shamil Akhmadullin <s.akhmadullin@netris.ru>
2023-08-24 23:53:07 +1000

    Implemented the feature: The project files, SPM libraries, assets set.

---
+++ TipCalculator/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TipCalculator
+//
+//  Created by Shamil on 24/8/2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
