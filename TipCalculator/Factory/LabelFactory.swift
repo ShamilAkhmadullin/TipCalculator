354a1ab4e76c81a8cb8461bfce660acd9397001f
Shamil Akhmadullin <s.akhmadullin@netris.ru>
2023-08-25 15:54:05 +1000

    Implemented the feature: Logo view details added.

---
+++ TipCalculator/Factory/LabelFactory.swift
@@ -0,0 +1,25 @@
+//
+//  LabelFactory.swift
+//  TipCalculator
+//
+//  Created by Shamil on 25/8/2023.
+//
+
+import UIKit
+
+struct LabelFactory {
+    static func build(
+        text: String?,
+        font: UIFont,
+        backgroundColor: UIColor = .clear,
+        textColor: UIColor = ThemeColor.text,
+        textAlingment: NSTextAlignment = .center) -> UILabel {
+            let label = UILabel()
+            label.text = text
+            label.font = font
+            label.backgroundColor = backgroundColor
+            label.textColor = textColor
+            label.textAlignment = textAlingment
+            return label
+        }
+}
--- TipCalculator/View/LogoView.swift
+++ TipCalculator/View/LogoView.swift
@@ -9,6 +9,48 @@ import UIKit
 
 class LogoView: UIView {
     
+    private let calculatorImageView: UIImageView = {
+        let imageView = UIImageView(image: .init(named: "Calculator"))
+        imageView.contentMode = .scaleAspectFit
+        return imageView
+    }()
+    
+    private let topLabel: UILabel = {
+        let label = UILabel()
+        let text = NSMutableAttributedString(string: "Mr TIP", attributes: [.font: ThemeFont.demiBold(ofSize: 16)])
+        text.addAttributes([.font: ThemeFont.bold(ofSize: 24)], range: NSMakeRange(3, 3))
+        label.attributedText = text
+        return label
+    }()
+    
+    private let bottomLabel: UILabel = {
+        LabelFactory.build(
+            text: "Calculator",
+            font: ThemeFont.demiBold(ofSize: 20),
+            textAlingment: .left)
+    }()
+    
+    private lazy var verticalStackView: UIStackView = {
+        let stackView = UIStackView(arrangedSubviews: [
+            topLabel,
+            bottomLabel
+        ])
+        stackView.axis = .vertical
+        stackView.spacing = -4
+        return stackView
+    }()
+    
+    private lazy var horizontalStackView: UIStackView = {
+        let stackView = UIStackView(arrangedSubviews: [
+            calculatorImageView,
+            verticalStackView
+        ])
+        stackView.axis = .horizontal
+        stackView.spacing = 8
+        stackView.alignment = .center
+        return stackView
+    }()
+    
     init() {
         super.init(frame: .zero)
         layout()
@@ -19,6 +61,13 @@ class LogoView: UIView {
     }
     
     private func layout() {
-        backgroundColor = .red
+        addSubview(horizontalStackView)
+        horizontalStackView.snp.makeConstraints { make in
+            make.top.bottom.equalToSuperview()
+            make.centerX.equalToSuperview()
+        }
+        calculatorImageView.snp.makeConstraints { make in
+            make.height.equalTo(calculatorImageView.snp.width)
+        }
     }
 }
